12dab2d9f980f802fa2204708f2e3033be175526
SarahOliverO <sarah.oliver1996@gmail.com>
2024-03-05 22:03:16 -0300

    Conteúdo do dia 05-02-2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/sarah/secao02/Text01/Text01.java
@@ -0,0 +1,22 @@
+package br.com.senaisp.bauru.sarah.secao02.Text01;
+
+public class Text01 {
+	/**
+	 * Comentario
+	 * @param args
+	 */
+    public static void main(String[] args) {
+    	//Comentario de uma linha
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        /**
+         * comentario de duas linha
+         * */
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+    }    
+}
