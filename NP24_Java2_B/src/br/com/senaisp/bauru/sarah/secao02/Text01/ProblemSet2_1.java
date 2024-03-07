3fd496cf7f5897627edbd2e5ba288acbf5c059c4
SarahOliverO <sarah.oliver1996@gmail.com>
2024-03-06 22:07:39 -0300

    Conteudo da aula do dia 06-03-2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/sarah/secao02/Text01/ProblemSet2_1.java
@@ -0,0 +1,19 @@
+package br.com.senaisp.bauru.sarah.secao02.Text01;
+
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        //Use 8 print statements to print a smiley face.
+        //Your art will rely on only a single character, besides space, such as X or #.
+    	//                  12345678
+        System.out.println("  ████  ");
+        System.out.println(" █    █ ");
+        System.out.println("█ █  █ █");
+        System.out.println("█      █");
+        System.out.println("█ █  █ █");
+        System.out.println("█  ██  █");
+        System.out.println(" █    █ ");
+        System.out.println("  ████  ");
+        
+      
+    }   
+}
