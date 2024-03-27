7061ef60e6e8b5e270f4ef3365ed3a41d237afc7
SarahOliverO <sarah.oliver1996@gmail.com>
2024-03-26 22:03:24 -0300

    Conteúdo da aula do dia 26-03-2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/sarah/licao06/lacofor/Exercicio02.java
@@ -0,0 +1,17 @@
+package br.com.senaisp.bauru.sarah.licao06.lacofor;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o número que deseja a tabuada:");
+		int num = sc.nextInt();
+		for(int i=1;i<=12;i++) {
+			System.out.println(num + " X " + i + " = " +  (num*i));
+		}
+		sc.close();
+	}
+
+}
